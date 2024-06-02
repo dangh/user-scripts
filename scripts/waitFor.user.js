ce0aeb444f8b8487f196b8ae6fcb482b5c3a8cd3
dangh <huynhminhdang@gmail.com>
2024-06-03 02:01:19 +0800

    add test function to check waitFor result

--- scripts/waitFor.user.js
+++ scripts/waitFor.user.js
@@ -2,20 +2,21 @@
 // @name         util::waitFor
 // @downloadURL  https://github.com/dangh/user-scripts/raw/master/scripts/waitFor.user.js
 // @match        <all_urls>
-// @version      0.0.1
+// @version      0.0.2
 // @run-at       document-start
 // ==/UserScript==
 
 unsafeWindow.waitFor = function waitFor(selector, opts) {
   return new Promise(resolve => {
     let container = opts?.container ?? document;
-    let interval = opts?.interval || 100;
-
     if (container?.tagName == 'IFRAME') container = container.contentDocument;
+    let interval = opts?.interval || 100;
+    let test = opts?.test ?? (el => true);
 
     let timer, stopObserver;
 
     stopObserver = unsafeWindow.observeAdd(selector, el => {
+      if (!test(el)) return;
       stopObserver();
       clearInterval(timer);
       resolve(el);
@@ -24,6 +25,7 @@ unsafeWindow.waitFor = function waitFor(selector, opts) {
     timer = setInterval(() => {
       let el = container.querySelector(selector);
       if (el) {
+        if (!test(el)) return;
         stopObserver();
         clearInterval(timer);
         resolve(el);
