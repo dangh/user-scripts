98e73344f2e8bfe036273e9187dcc02acc2dd0b1
Dang <huynhminhdang@gmail.com>
2025-03-09 23:56:37 +0800

    favicon

---
+++ scripts/favicon.user.js
@@ -0,0 +1,70 @@
+// ==UserScript==
+// @name         util::favicon
+// @downloadURL  https://github.com/dangh/user-scripts/raw/master/scripts/favicon.user.js
+// @match        <all_urls>
+// @version      0.0.1
+// @run-at       document-start
+// ==/UserScript==
+
+unsafeWindow.favicon = function favicon(svg, opts) {
+  if (svg) {
+    switch (svg) {
+      case 'progress': svg = progress(opts); break;
+      case 'emoji': svg = emoji(opts); break;
+    }
+    let link = document.querySelector('link[favicon]');
+    if (!link) {
+      link = document.createElement('link');
+      link.rel = 'icon';
+      document.head.append(link);
+    }
+    svg = svg.replace(/[\r\n]g/, '');
+    svg = svg.replace(/>\s+/g, '>');
+    svg = svg.replace(/\s+</g, '<');
+    svg = svg.replace(/[#]/g, encodeURIComponent);
+    link.href = 'data:image/svg+xml,' + svg;
+  } else {
+    document.querySelector('link[favicon]')?.remove();
+    document.head.querySelectorAll('link:is([rel~="icon"])').forEach(link => {
+      link.remove();
+      document.head.append(link);
+    });
+  }
+};
+
+unsafeWindow.favemoji = function favemoji(icon, badge) {
+  if (!icon) unsafeWindow.favicon(null);
+  else unsafeWindow.favicon('emoji', { icon, badge });
+};
+
+function progress(opts) {
+  let percent = opts.percent ?? 0;
+  let width = opts.width ?? 6;
+  let trackColor = opts.trackColor ?? '#e0e0e0';
+  let progressColor = opts.progressColor ?? '#60e6a8';
+  let shape = opts.shape ?? 'butt';
+
+  let size = 32, center = size / 2, radius = (size - width) / 2;
+  let circumference = 2 * Math.PI * radius;
+  let offset = circumference * (1 - percent);
+  let svg = `
+    <svg width="${size}" height="${size}" viewBox="0 0 ${size} ${size}" xmlns="http://www.w3.org/2000/svg">
+      <circle r="${radius}" cx="${center}" cy="${center}" fill="transparent" stroke="${trackColor}" stroke-width="${width}"></circle>
+      <circle id="progress" r="${radius}" cx="${center}" cy="${center}" fill="transparent" stroke="${progressColor}" stroke-linecap="${shape}" stroke-width="${width}" stroke-dasharray="${circumference}" stroke-dashoffset="${offset}" transform="rotate(-90)" transform-origin="center"></circle>
+    </svg>
+  `;
+  return svg;
+}
+
+function emoji(opts) {
+  let { icon, badge } = opts;
+  if (icon) icon = `<text y=".9em" font-size="90">${icon}</text>`;
+  if (badge) badge = `<text x="1em" y="2.1em" font-size="45">${badge}</text>`;
+  let svg = `
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
+      ${icon}
+      ${badge}
+    </svg>
+  `;
+  return svg;
+}
