6e034da2f3e77d40170d3cf503026e6b3547afa1
Dang <huynhminhdang@gmail.com>
2025-03-26 01:22:05 +0800

    replaceTextContent: support updating replaced nodes

--- scripts/replaceTextContent.user.js
+++ scripts/replaceTextContent.user.js
@@ -2,11 +2,11 @@
 // @name         util::replaceTextContent
 // @downloadURL  https://github.com/dangh/user-scripts/raw/master/scripts/replaceTextContent.user.js
 // @match        <all_urls>
-// @version      0.0.1
+// @version      0.0.2
 // @run-at       document-start
 // ==/UserScript==
 
-unsafeWindow.replaceTextContent = function replaceTextContent(container, pattern, replacer) {
+unsafeWindow.replaceTextContent = function replaceTextContent(container, pattern, replacer, updater) {
   //collect all text nodes
   let treeWalker = document.createTreeWalker(container, NodeFilter.SHOW_TEXT);
   let allTextNodes = [];
@@ -22,11 +22,15 @@ unsafeWindow.replaceTextContent = function replaceTextContent(container, pattern
       if (!id) return match;
 
       let attr = 'ts-replacer-' + id;
-      let replaced = el.parentNode.closest(`[${attr}]`) != null;
-      if (replaced) return match;
+      let replacedBy = el.parentNode.closest(`[${attr}]`);
+      if (replacedBy != null) {
+        if (updater) updater(replacedBy, match);
+        return match;
+      }
 
       let content = replacer;
       if (typeof replacer == 'function') content = replacer(match, ...args);
+      if (content == match) return match;
       return `<span ${attr}>${content}</span>`;
     });
     if (html != el.textContent) {
